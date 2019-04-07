0b607730afc4e67d624df7953aed5b58734d02f7
Lou <info@loujoseph.ca>
2019-04-07 19:05:05 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Louis Joseph on 2019-04-06.
+//  Copyright © 2019 Louis Joseph. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
